bc23725ecb2c3e84eaa488a010ef21e01f2bce71
Max Tharr <max.tharr@mayflower.de>
2020-01-21 14:20:06 +0100

    Initial Commit

---
+++ iCospend/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCospend
+//
+//  Created by Max Tharr on 21.01.20.
+//  Copyright © 2020 Mayflower GmbH. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
