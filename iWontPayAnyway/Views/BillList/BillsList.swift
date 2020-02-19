868c29e5b4b5e8563ac65c1e732029ccb719b709
Max Tharr <max.tharr@mayflower.de>
2020-02-19 14:24:38 +0100

    Fix formatting

--- iWontPayAnyway/Views/BillList/BillsList.swift
+++ iWontPayAnyway/Views/BillList/BillsList.swift
@@ -19,20 +19,23 @@ struct BillsList: View {
     var body: some View {
         NavigationView {
             List {
-                    ForEach(viewModel.currentProject.bills.sorted(by: {
-                        $0.lastchanged > $1.lastchanged
-                    })) { bill in
-                        NavigationLink(destination:
-                            BillDetailView(showModal: .constant(false),
-                                           viewModel: self.viewModel,
-                                           currentBill: bill,
-                                           navBarTitle: "Edit Bill",
-                                           owers: self.viewModel.initOwers(currentBill: bill))) {
-                                            BillCell(viewModel: self.viewModel, bill: bill)
-                        }
+                ForEach(viewModel.currentProject.bills.sorted(by: {
+                    $0.lastchanged > $1.lastchanged
+                })) { bill in
+                    NavigationLink(destination:
+                        BillDetailView(showModal: .constant(false),
+                                       viewModel: self.viewModel,
+                                       currentBill: bill,
+                                       navBarTitle: "Edit Bill",
+                                       owers: self.viewModel
+                                        .initOwers(currentBill: bill))) {
+                                            BillCell(
+                                                viewModel: self.viewModel,
+                                                     bill: bill)
                     }
-                .onDelete(perform: deleteBill)
                 }
+                .onDelete(perform: deleteBill)
+            }
             .navigationBarTitle("Bills")
         }
         .onAppear {
