c19642d4e9ea5e33942ca1f0da885ba6fe5d2952
Max Tharr <max.tharr@mayflower.de>
2020-10-03 11:32:30 +0200

    Restructure Floating Button

--- PayForMe/Util/FloatingAddButtonViewModifier.swift
+++ PayForMe/Util/FloatingAddButtonViewModifier.swift
@@ -9,27 +9,30 @@
 import SwiftUI
 
 struct FloatingAddButtonViewModifier: ViewModifier {
-    @State var sheetToggle = false
+    @State private var sheetToggle = false
     
     func body(content: Content) -> some View {
-        ZStack {
-            content
-            VStack {
-                Spacer()
-                Button(action: {
-                    sheetToggle.toggle()
-                }) {
-                    Image(systemName: "plus.circle")
-                        .resizable()
-                        .frame(width: 64, height: 64)
-                        .foregroundColor(Color.secondary)
-                        .shadow(radius: 10)
-                }
-            }.padding(EdgeInsets(top: 0, leading: 32, bottom: 64, trailing: 32))
-        }
-        .sheet(isPresented: $sheetToggle) {
-            AddBillView(showModal: $sheetToggle)
-        }
+        content
+            .sheet(isPresented: $sheetToggle) {
+                AddBillView(showModal: $sheetToggle)
+            }
+            .overlay( FloatingAddButtonView(sheetToggle: $sheetToggle).padding(32), alignment: .bottom)
+    }
+}
+
+private struct FloatingAddButtonView: View {
+    @Binding var sheetToggle: Bool
+    
+    var body: some View {
+            Button(action: {
+                sheetToggle.toggle()
+            }) {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .frame(width: 48, height: 48)
+                    .foregroundColor(Color.secondary)
+                    .shadow(radius: 10)
+            }
     }
 }
 
