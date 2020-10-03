cb8e702ebc651574e4510b9d2a373e2bcb6256fc
Max Tharr <max.tharr@mayflower.de>
2020-10-03 13:12:29 +0200

    Redesign onboarding experience

--- PayForMe/Views/Projects/OnboardingView.swift
+++ PayForMe/Views/Projects/OnboardingView.swift
@@ -10,15 +10,73 @@ import SwiftUI
 
 struct OnboardingView: View {
     
-    @State var isQRScanning = false
+    @State private var moreInfo = false
     
     var body: some View {
         NavigationView {
-            if isQRScanning {
-                AddProjectQRView()
-            } else {
-                ProjectDetailView()
-            }
+            VStack(spacing: 32) {
+                Text("Welcome to PayForMe!").font(.largeTitle)
+                Text("To get started sharing expenses with friends, you must add a project from Cospend or iHateMoney. \n\n Cospend projects can be added by QR Code or by manually filling out the information, for iHateMoney you need to fill out the infromation manually.")
+                HStack(spacing: 50) {
+                    NavigationLink(destination: ProjectQRPermissionCheckerView()) {
+                        Image(systemName: "qrcode")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    NavigationLink(
+                        destination: ProjectDetailView(),
+                        label: {
+                            Image(systemName: "square.and.pencil")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        })
+                }.padding(.horizontal, 30)
+                if moreInfo {
+                    Button(action: {
+                        withAnimation {
+                            self.moreInfo.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: "chevron.compact.up")
+                            .resizable().aspectRatio(contentMode: .fit).frame(width:30)
+                    })
+                    HStack(alignment: .top, spacing: 12) {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Cospend is a NextCloud app")
+                            Button("Go to nextcloud") {
+                                if let url = URL(string: "https://nextcloud.com/") {
+                                    if UIApplication.shared.canOpenURL(url) {
+                                        UIApplication.shared.open(url)
+                                    }
+                                }
+                            }
+                        }
+                        Spacer()
+                        VStack(alignment: .trailing, spacing: 5) {
+                            Text("To use iHateMoney, host an own instance or register at").multilineTextAlignment(.trailing)
+                            Button("iHateMoney.org") {
+                                if let url = URL(string: "https://ihatemoney.org/") {
+                                    if UIApplication.shared.canOpenURL(url) {
+                                        UIApplication.shared.open(url)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    Button(action: {
+                        withAnimation {
+                            self.moreInfo.toggle()
+                        }
+                    }, label: {
+                        Image(systemName: "questionmark")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(height: 40)
+                    })
+                }
+                Spacer()
+            }.padding(20)
         }
     }
 }
